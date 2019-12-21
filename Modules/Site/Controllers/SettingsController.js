a0692393faaee55e6f53a3caa0e5a2af34f05cf5
adedotunbashorun <adedotunolawale@gmail.com>
2019-12-21 11:46:46 +0100

    minor fix on settings

--- Modules/Site/Controllers/SettingsController.js
+++ Modules/Site/Controllers/SettingsController.js
@@ -5,59 +5,50 @@ const Pusher = require('../../../functions/pusher')
 const File = require('../../../functions/file')
 class SystemSettingsController {
     
-    static create(req, res, next) {
+    static async create(req, res, next) {
         try {
             let setting = {
                 data: req.body
             }
             if(req.body._id !== ""){
-                SystemSettings.findOneAndUpdate({_id: req.body._id }, setting ,{upsert: true, new: true},function(error, settings) {
-                    if (error) {
-                        return res.status(401).json({ error: error, msg: error.message })
-                    } else {
-                        settings.data.logo = (req.body.logo) ? File.Image(req.body.logo,"/images/settings/", req.body.app_name,'.png') : settings.data.logo
-                        settings.save()
-                        Pusher.triggerNotification('notifications','settings',{settings,message:{msg: req.user + " updated settings."}},req,req.userId)
-                        return res.status(201).json({ settings: settings, msg: 'Settings Successfully updated.' })
-                    }
-                })
-            }else{
-                var settings = new SystemSettings(setting)
-                settings.image_url = (req.body.image) ? File.Image(req.body.image,"/images/class/", req.body.name,'.png') : ''
+                
+                let settings =  await SystemSettings.findById(req.body.id)
+                setting.data = req.body
+                settings.data.logo = (req.body.logo) ? File.Image(req.body.logo,"/images/settings/", req.body.app_name,'.png') : settings.data.logo
                 settings.save()
-                settings.save(function(error) {
-                    if (error) {
-                        return res.status(401).json({ error: error, msg: error.message })
-                    } else {
-                        Pusher.triggerNotification('notifications','settings',{settings,message:{msg: req.user+" created settings."}},req, req.userId)
-                        return res.status(201).json({ settings: settings, msg: 'Settings Successfully saved.' })
-                    }
-                })
+                Pusher.triggerNotification('notifications','settings',{settings,message:{msg: req.user + " updated settings."}},req,req.userId)
+                return res.status(201).json({ settings: settings, msg: 'Settings Successfully updated.' })
+                    
+            }else{
+
+                var settings = new SystemSettings(setting)
+                settings.data.image_url = (req.body.image) ? File.Image(req.body.image,"/images/class/", req.body.name,'.png') : ''
+                await settings.save()
+                Pusher.triggerNotification('notifications','settings',{settings,message:{msg: req.user+" created settings."}},req, req.userId)
+                return res.status(201).json({ settings: settings, msg: 'Settings Successfully saved.' })
             }
         } catch (error) {
             return res.status(422).json({ error: error, msg: error.message })
         }
     }
 
-    static getAll(req, res, next) {
+    static async getAll(req, res, next) {
         try {
-            SystemSettings.findOne({}, null, { sort: { 'createdAt': -1 } }).then((settings) => {
-                return res.status(201).json({ settings: settings })
-            }, (error) => {
-                return res.status(501).json({ "success": false, "message": error })
-            })
+
+            let settings = await SystemSettings.findOne({}).sort('-createdAt')
+            return res.status(201).json({ settings: settings })
+
         } catch (err) {
             return res.status(500).json(err)
         }
     }
 
-    static getOne(req, res, next) {
+    static async getOne(req, res, next) {
         try {
-            SystemSettings.findOne({ _id: req.params.id }, null, { sort: { 'createdAt': -1 } }).then((settings) => {
-                return res.status(201).json({ settings: settings })
-            }, (error) => {
-                return res.status(501).json({ "success": false, "message": error })
-            })
+
+            let settings = await SystemSettings.findById(req.params.id)
+            return res.status(201).json({ settings: settings })
+            
         } catch (err) {
             return res.status(500).json(err)
         }
